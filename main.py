19e2b0c87f0732f68151eae94dcc99b35f6f6e2a
sumansamanta <68455786+sumansamanta@users.noreply.github.com>
2021-01-01 19:59:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import json
 import threading 
-#this is for python 3.0 and above. use import thread for python2.0 versions
+
 from threading import*
 import time
 
-d={} #'d' is the dictionary in which we store data
+d={}               #'d' is the dictionary in which we store data
 
 #for create operation 
 #use syntax "create(key_name,value,timeout_value)" timeout is optional you can continue by passing two arguments without timeout
@@ -15,44 +16,44 @@ def create(key,value,timeout=0):
         if(key.isalpha()):
             if len(d)<(1024*1020*1024) and value<=(16*1024*1024): #constraints for file size less than 1GB and Jasonobject value less than 16KB 
                 if timeout==0:
-                    l=[value,timeout]
+                    l1=[value,timeout]
                 else:
-                    l=[value,time.time()+timeout]
+                    l1=[value,time.time()+timeout]
                 if len(key)<=32: #constraints for input key_name capped at 32chars
                     d[key]=l
+                    print(key+" is created in database")
             else:
                 print("error: given Memory limit is  exceeded!! ")#error message2
         else:
             print("error: Invalind key name!! key name must contain only alphabets and no special characters or numbers")#error message3
 
 #for read operation
-#use syntax "read(key_name)"
             
 def read(key):
     if key not in d:
         print(" given key does not exist in database. Please enter a valid key") #error message4
     else:
-        b=d[key]
-        if b[1]!=0:
-            if time.time()<b[1]: #comparing the present time with expiry time
-                stri=str(key)+":"+str(b[0]) #to return the value in the format of JasonObject i.e.,"key_name:value"
-                return stri
+        p=d[key]
+        if p[1]!=0:
+            if time.time()<p[1]: #comparing the present time with expiry time
+                fresh=str(key)+":"+str(p[0]) #to return the value in the format of JasonObject i.e.,"key_name:value"
+                return fresh
             else:
                 print("error: time expierd for",key) # show error message5
         else:
-            stri=str(key)+":"+str(b[0])
-            return stri
+            fresh=str(key)+":"+str(p[0])
+            return fresh
 
 #for delete operation
-#use syntax "delete(key_name)"
+
 
 def delete(key):
     if key not in d:
         print(" given key does not exist in database. Please enter a valid key") #error message4
     else:
-        b=d[key]
-        if b[1]!=0:
-            if time.time()<b[1]: #comparing the current time with expiry time
+        p=d[key]
+        if p[1]!=0:
+            if time.time()<p[1]: #comparing the current time with expiry time
                 del d[key]
                 print("key is successfully deleted from database")
             else:
